262b15a829a7d9d0b9cb3f2b6fb25e35040fe2fb
Shay <sgaming24@outlook.com>
2022-02-21 09:25:50 +0000

    Crash max = 899.99x

--- utils/crash.js
+++ utils/crash.js
@@ -1,7 +1,10 @@
 const config = require("../config.json");
 
 exports.generateMultiplier = function() {
-    i = Math.random();
+    i = parseFloat(Math.random().toFixed(3));
+    while (i == 1) {
+        i = parseFloat(Math.random().toFixed(3));
+    };
     i = 0.9/(1-i);
     i = Math.max(i,1.0);
     i = Math.floor(i*100)/100; // multiplier
