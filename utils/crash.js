40036b63dedc7e5ba252133bad4cfda2ee124fe3
Shay <sgaming24@outlook.com>
2022-02-23 11:08:41 +0000

    Exterminate Math.random() (crash.js)

--- utils/crash.js
+++ utils/crash.js
@@ -2,11 +2,10 @@ const Random = require("crypto-random");
 const config = require("../config.json");
 
 exports.generateMultiplier = function() {
-    let ret = Random.range(0, 1000);
-    i = ret / 1000;
+    i = Random.range(0, 1000) / 1000;
     // i = parseFloat(Math.random().toFixed(3));
     while (i == 1) {
-        i = parseFloat(Math.random().toFixed(3));
+        i = Random.range(0, 1000) / 1000;
     };
     i = 0.9/(1-i);
     i = Math.max(i,1.0);
